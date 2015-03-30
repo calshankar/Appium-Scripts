496d11354b77f79d8672d7a0d63adbba2aeb4546
callshank_78@yahoo.com <callshank_78@yahoo.com>
2015-03-30 17:45:59 +0530

    Adding Safari Browser Scripts to be run on Simulator Only

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Appium-Scripts
-Random Appium scripts
+I have added some Random Appium scripts to get you started some of the basic. Post in your comments for any questions
+
+  1. safari-scripts - These scripts are for iOS platform to be run on Safari Browser. Please run this script on           Simulator only
---
+++ Safari-scripts/safariBrowser.py
@@ -0,0 +1,58 @@
+__author__ = 'Shankar'
+
+import unittest
+import sys
+from appium import webdriver
+import json
+import logging
+from time import sleep
+from pprint import pprint
+from AppiumLibrary.keywords import _ApplicationManagementKeywords
+
+logger = logging.getLogger()
+stream_handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(stream_handler)
+stream_handler = logging.StreamHandler(sys.stderr)
+logger.addHandler(stream_handler)
+
+class safariTest(unittest.TestCase):
+
+    def setUp(self):
+        with open('/Users/aahlaadlabmac/PycharmProjects/Appium/config.json') as config_file:
+            config = json.load(config_file)
+
+        desired_caps = config['safariSimulator']
+        '''pprint(desired_caps) For checking Desired capability'''
+        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+        self.driver.implicitly_wait(2)
+
+    def tearDown(self):
+        sleep(3)
+        self.driver.quit()
+
+    def test_login(self):
+
+        try:
+            self.driver.get("http://beta.housejoy.in/promotions/plumbing")
+            sleep(6)
+            usrname = self.driver.find_element_by_id('userName')
+            usrname.click()
+            sleep(1)
+            usrname.send_keys('shankar')
+            mobile = self.driver.find_element_by_id('mobileNumber')
+            mobile.click()
+            mobile.send_keys('9448083238')
+            sleep(1)
+            mailid = self.driver.find_element_by_id('userEmailid')
+            mailid.send_keys('shanky@hotmail.com')
+            sleep(10)
+            button = self.driver.find_element_by_xpath("//span[1][contains(text(),'Book a Plumber No')]")
+            button.click()
+            sleep (8)
+            self.driver.back()
+        except stream_handler:
+            print("failed to Execute the Command")
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(safariTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
